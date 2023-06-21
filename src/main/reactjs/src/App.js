d02ef05af6a3426ab49fa822b73c7218e47d3590
Xoohyun <ehowl3609@gmail.com>
2023-06-21 16:30:55 +0900

    first commit

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import img from './다운로드 (1).png';
+
+function App() {
+  return (
+    <div className="App">
+
+      <img src={img} alt='' width={300}/>
+      <br/>
+      <h5>Docker 배포 실습</h5>
+      
+    </div>
+  );
+}
+
+export default App;
